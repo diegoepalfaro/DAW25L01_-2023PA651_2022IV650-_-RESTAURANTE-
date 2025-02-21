b68925b274b1c11b21bc97bc9f924ba6adfc80bc
marlon villeda <marloninteriano35@gmail.com>
2025-02-20 18:32:08 -1200

    nuevos cambios

--- L01_2023PA651_2022IV650/Models/Plato.cs
+++ L01_2023PA651_2022IV650/Models/Plato.cs
@@ -4,10 +4,10 @@ namespace L01_2023PA651_2022IV650.Models
     public class Plato
     {
         [Key] 
-        public int platoId { get; set; }
+        public int PlatoId { get; set; }
 
-        public string nombrePlato { get; set; }
+        public string NombrePlato { get; set; }
 
-        public decimal precio { get; set; }
+        public decimal Precio { get; set; }
     }
 }
