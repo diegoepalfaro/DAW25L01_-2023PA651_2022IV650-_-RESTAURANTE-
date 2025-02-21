e821ae9260ee9b970ccf57e08e301321eec2ef0c
diegoepalfaro <diegoepalfaro@gmail.com>
2025-02-20 18:28:51 -0600

    Agregando controladores

---
+++ L01_2023PA651_2022IV650/Controllers/ClientesController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using L01_2023PA651_2022IV650.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
+
+namespace L01_2023PA651_2022IV650.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        private readonly RestauranteContext _RestauranteContexto;
+
+        public ClienteController(RestauranteContext RestauranteContexto)
+        {
+            _RestauranteContexto = RestauranteContexto;
+        }
+
+        ///<sumary>
+        ///Endpoint que retorna el listado de todos los clientes existentes
+        ///</sumary>
+        ///<returns></returns>
+        [HttpGet]
+        [Route("GetAll")]
+
+        public IActionResult Get()
+        {
+
+            List<Clientes> listadoLibros = (from e in _RestauranteContexto.Clientes
+                                            select e).ToList();
+
+            if (listadoLibros.Count() == 0)
+            {
+                return NotFound();
+            }
+            return Ok(listadoLibros);
+        }
+
+    }
+}
--- L01_2023PA651_2022IV650/Models/RestauranteContext.cs
+++ L01_2023PA651_2022IV650/Models/RestauranteContext.cs
@@ -10,6 +10,8 @@ namespace L01_2023PA651_2022IV650.Models
         public RestauranteContext(DbContextOptions<RestauranteContext> options) : base(options) 
         { 
         }
+
+        public DbSet<Pedidos> Pedidos { get; set; }
         public DbSet<Clientes> Clientes { get; set; }
         public DbSet<Plato> Platos { get; set; }
 
