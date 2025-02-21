3cc13ac0e36541bde91ef6a6e184ebcb5236ae7d
marlon villeda <marloninteriano35@gmail.com>
2025-02-20 17:53:50 -1200

    ya se descargaron los paquetes

--- L01_2023PA651_2022IV650/models/Plato.cs
+++ L01_2023PA651_2022IV650/models/Plato.cs
@@ -1,6 +1,14 @@
-﻿namespace L01_2023PA651_2022IV650.models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace L01_2023PA651_2022IV650.Models
 {
     public class Plato
     {
+        [Key] 
+        public int PlatoId { get; set; }
+
+        public string NombrePlato { get; set; }
+
+        public decimal Precio { get; set; }
     }
 }
